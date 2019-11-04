dda0a831fec5cef705a5531a25351606ea30c76a
minaammunir <minaammunir@gmail.com>
2019-11-04 05:39:28 +0500

    fixed bugs

--- frontend/src/app/classes/ServerInfo.ts
+++ frontend/src/app/classes/ServerInfo.ts
@@ -1,8 +1,8 @@
 export class ServerInfo
 {
-    public static Url = 'http://localhost/tasks/backend/public'
+    public static Url = 'http://sockoyetasks.akonto.ltd/backend/public'
 
-    public static secret = 'RN0eiAR5KUDigm053LPcIxh89z3kB1eowlNcS1vB';
+    public static secret = 'szD94c0FTwuF4GE6rtOGGyje1k9jZLeHoqKRgimB';
 
     public static clientId = 2;
 }
